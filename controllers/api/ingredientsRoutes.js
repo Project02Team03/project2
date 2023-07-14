ee0db05aa25596b6f82574263ecbc646bb1b44d5
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2023-07-14 17:53:06 -0500

    PUT ingredients route

--- controllers/api/ingredientsRoutes.js
+++ controllers/api/ingredientsRoutes.js
@@ -11,7 +11,7 @@ const withAuth = require('../../utils/auth');
 
 //get all ingredients
 router.get("/", (req, res) => {
-   console.log('HI THERE');
+   //console.log('HI THERE');
    
     Ingredients.findAll({
             attributes: ["id", "ingredient_name", "amount", "in_stock"],
@@ -23,20 +23,45 @@ router.get("/", (req, res) => {
             res.status(500).json(err);
         });
 });
-//get all ingredients from one recipe?
-router.get("/recipe/:id", (req, res) => {
-    const { id } = req.params;
-    Ingredients.findByPk({ where: { id } })
-      .then((ingredientsData) => {
-        if (ingredientsData.length === 0) {
-          res.status(404).json({ message: `No ingredients found` });
-          return;
-        }
-        res.status(200).json(ingredientsData);
-      })
-      .catch((err) => {
-        res.status(500).json(err);
+
+//update ingredient by its id , ex: in_stock: true
+router.put('/:id', async(req,res) =>{
+  //console.log('---------LETS UPDATE-------------------');
+  try{
+    const ingredientData=await Ingredients.update(
+      req.body, {
+        where: {
+          id: req.params.id,
+        },
       });
-  });
+      if (ingredientData){
+        res.status(200).json(ingredientData);
+      }else {
+        res.status(404).json.end();
+      }
+  } catch (err){
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+
+//get all ingredients from one recipe?  ---> in recipe-routes, get recipe(with ingredients) by id
+// router.get("/recipe/:id", (req, res) => {
+//     const { id } = req.params;
+//     Ingredients.findByPk({ where: { id } })
+//       .then((ingredientsData) => {
+//         if (ingredientsData.length === 0) {
+//           res.status(404).json({ message: `No ingredients found` });
+//           return;
+//         }
+//         res.status(200).json(ingredientsData);
+//       })
+//       .catch((err) => {
+//         res.status(500).json(err);
+//       });
+//   });
   
   module.exports = router;
