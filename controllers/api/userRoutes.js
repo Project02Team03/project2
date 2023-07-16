904a92d7abdfa46d20634a67b76652aff0ac9862
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2023-07-16 09:28:05 -0500

    update models and seeds

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Recipe , SelectedRecipe} = require('../../models');
+const withAuth=require('../../utils/auth')
 
 router.post('/login', async (req, res) => {
   try {
@@ -47,11 +48,50 @@ router.post('/logout', (req, res) => {
   }
 });
 
+//sing up new user
+//CREATE new user after Sign Up
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    // Set up sessions with the 'loggedIn' variable
+    req.session.save(() => {
+      // Set the 'loggedIn' session variable to 'true'
+      req.session.loggedIn=true
+    if (req.session.loggedIn){
+      return true;
+    }
+    res.render("homepage", {
+       loggedIn: req.session.loggedIn
+    }
+    )
+      res.status(200).json(dbUserData);
+  });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//all users
+router.get('/', async (req,res) => {
+  try{
+    const allUsersData=await User.findAll();
+    res.status(200).json(allUsersData)
+  } catch(err){
+    res.status(500).json(err);
+  }
+});
+
 //all recipes, favorited by logged in user????? we can add auth later
-router.get('/:id', async(req,res) => {
+router.get('/:id',withAuth, async(req,res) => {
   try {
     const myRecipes=await User.findByPk(req.params.id, {
-      
+      where:{id: req.session.id},
       include: 
       {model: Recipe, through: SelectedRecipe, as: 'recipes'},
       attributes: { exclude: ['email','password'] }
@@ -61,6 +101,7 @@ router.get('/:id', async(req,res) => {
     res.status(500).json(err);
   }
 });
+
 //all users
 router.get('/', async(req,res) => {
   try {
@@ -72,6 +113,8 @@ router.get('/', async(req,res) => {
     res.status(500).json(err);
   }
 });
+
+
 
 
 module.exports = router;
--- public/js/signup.js
+++ public/js/signup.js
@@ -5,7 +5,7 @@ const signupFormFunction = async (event) => {
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    const response = await fetch('/api/user', {
+    const response = await fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({
             username, 
