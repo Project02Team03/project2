5fc38accb9a1f044cb1fa715f2a58b773cb8c53a
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-07-17 10:17:09 -0500

    Third attempt to get recipe to render

--- public/js/recipeModal.js
+++ public/js/recipeModal.js
@@ -2,17 +2,20 @@ const renderModal = async (event) => {
     event.preventDefault();
 
     const id = event.target.getAttribute("data-recipe");
-    console.log(id);
+    console.log("THIS IS THE RECIPE ID", id);
 
     try {
         const response = await fetch(`/api/recipes/${id}`);
+
+        console.log(response);
+
         if (response.ok) {
             const recipe = await response.json();
             const template = Handlebars.compile(document.getElementById('recipe-modal').innerHTML);
             const renderedHTML = template(recipe);
             document.getElementById('recipeModalContent').innerHTML = renderedHTML;
         } else {
-            throw new Error(response.statusText);
+            alert(response.statusText);
         }
     } catch (err) {
         console.error(err);
