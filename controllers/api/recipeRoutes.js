56af2efa9a5bb99434c37b99899f60213ce0bc04
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2023-07-26 18:14:38 -0500

    develop with adding /removing favs

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -132,47 +132,12 @@ router.get("/recipe", async (req, res) => {
 
 
 //then i need to get a single recipe
-router.get("/:id", withAuth, async (req, res) => {
+router.get("/:id", async (req, res) => {
 
     try {
-        if (req.session) {
-            const RecipeData = await Recipe.findOne({  
-                where: {
-                    id: req.params.id,
-                },
-                include:{model: Ingredients, through: ShoppingList, as: 'ingredientList'}
-            });  
-            if (!RecipeData) {
-                res.status(404).json({
-                    message: "Not found"
-                });
-                return;
-            }
-    
-            const recipe = RecipeData.get({ plain: true });
-    
-            const recipeIngredients = await Ingredients.findAll({
-                where: {
-                    id: { [Op.ne]: req.params.id },
-                    ingredient_name: recipe.ingredientList.map((ingredient) => ingredient.ingredient_name),
-                },
-                /* 
-                include: {
-                    model: Ingredients,
-                    through: ShoppingList,
-                    as: 'ingredientList',
-                    where: {
-                        ingredient_name: recipe.ingredientList.map((ingredient) => ingredient.ingredient_name),
-                    },
-                },
-                 */
-            });
-            
-            
-            res.render("recipe-detail", {
-                recipe,
+        const RecipeData=await Recipe.findOne({  
+            where: {
                 id: req.params.id,
-
             },
             include: !req.session.logged_in ? {model: Ingredients, through: ShoppingList, as: 'ingredientList'} 
             :[{model: Ingredients, through: ShoppingList, as: 'ingredientList'}, {model: User, through: SelectedRecipe, as: 'users'}]
@@ -205,7 +170,6 @@ router.get("/:id", withAuth, async (req, res) => {
             
             logged_in: req.session.logged_in
         });
-
 
     }  catch(err){
         console.log(err);
